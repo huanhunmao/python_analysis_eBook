d6236caa3e9a304eeb9fbd564ce5ee9ddbc63f2c
Marxu <fuhaojun@52tt.com>
2024-06-27 21:14:32 +0800

    feat: 正则基本使用 来匹配 指定单词 所在的句子

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ with open('miracle_in_the_andes.txt') as file:
 # 使用正则匹配
 pattern = re.compile('Chapter [0-9]+')
 findings = re.findall(pattern, book)
-print(findings)
+# print(findings)
 # ['Chapter 1', 'Chapter 2', 'Chapter 3', 'Chapter 4', 'Chapter 5', 'Chapter 6',
 #  'Chapter 7', 'Chapter 8', 'Chapter 9', 'Chapter 10']
 
@@ -20,3 +20,24 @@ print(findings)
 pattern1 = re.compile('chapter [a-z]+')
 findings1 = re.findall(pattern1, book)
 # print(findings1) # ['chapter ab']
+
+
+# 找 哪个句子 使用了 love 这个词
+pattern2 = re.compile('love')
+findings2 = re.findall(pattern2, book)
+# print(findings2) # 答案是 错误的 ❌  找到的都是 单独的 love 单词
+
+# 怎么 ✅ 正确解决这个问题
+pattern3 = re.compile('[a-zA-Z]* love [a-zA-Z]*')
+findings3 = re.findall(pattern3, book)
+# print(findings3) # 找到些 'passionate love for',  这样的 接近答案了
+
+# 继续接近答案
+# ^. 表示排除.  意思是从上一句话末尾
+pattern4 = re.compile('[^.]*[^a-zA-Z]+love[^a-zA-Z]+[^.]*.')
+pattern5 = re.compile('[A-Z]{1}[^.]*[^a-zA-Z]+love[^a-zA-Z]+[^.]*.')
+findings4 = re.findall(pattern4, book)
+findings5 = re.findall(pattern5, book)
+# print(findings4)
+print(len(findings4)) # 67
+# print(findings5)
